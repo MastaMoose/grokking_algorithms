1b3ca8e390e95ab0e9f966a3f807516558360d4f
MastaMoose <mustafa.sheikhj@gmail.com>
2018-08-19 22:58:00 -0400

    Made some changes to chapter 6's implementation of breadth-first to port it to Python 3.x and also added print statements to trace execution.

--- 06_breadth-first_search/python/01_breadth-first_search.py
+++ 06_breadth-first_search/python/01_breadth-first_search.py
@@ -23,12 +23,14 @@ def search(name):
         # Only search this person if you haven't already searched them.
         if person not in searched:
             if person_is_seller(person):
-                print person + " is a mango seller!"
+                print(person + " is a mango seller!")
                 return True
             else:
                 search_queue += graph[person]
                 # Marks this person as searched
                 searched.append(person)
+        print(search_queue)
+        print(searched)
     return False
 
-search("you")
+print(search("you"))
